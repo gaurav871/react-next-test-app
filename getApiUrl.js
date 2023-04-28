3ec564722afabbffe9d2436d7ab1851e17e54f98
gaurav871 <131999852+gaurav871@users.noreply.github.com>
2023-04-27 20:41:53 -0700

    Update getApiUrl.js

--- getApiUrl.js
+++ getApiUrl.js
@@ -1,7 +1,7 @@
 export default function getApiUrl() {
   const url = {
     development: "http://localhost:3000",
-    production: "https://644aaf069e9eb20ee25c90ec--charming-taiyaki-9a13b8.netlify.app",
+    production: "https://charming-taiyaki-9a13b8.netlify.app",
   }
 
   const env = process.env.NODE_ENV || "development"
